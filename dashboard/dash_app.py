795cab8323436b6eb17e6efe8749e384b170625c
Ryan Singman <ryanjs@vt.edu>
2021-03-09 17:41:19 -0500

    added machine configs, added project skeleton, added utils submodule

--- dashboard/dash_app.py
+++ dashboard/dash_app.py
@@ -5,9 +5,7 @@ import zmq
 
 class DashApp:
     """Digital Dashboard web app class -- handles data ingestion and visualization"""
-    def __init__(
-        self, data_port: int, publish_port: int, machine_configs: Dict[Dict]
-    ):
+    def __init__(self, data_port: int, machine_configs: Dict[Dict]):
         """Initializes Dash app and layout, connects to data socket, sets up Dash callbacks
 
         Parameters
@@ -17,4 +15,10 @@ class DashApp:
         machine_configs : Dict[Dict]
             configuration dictionaries for each machine
         """
+        # set up data socket
+        self.context = zmq.Context()
+        self.data_socket = self.context.socket(zmq.PULL)
+        self.data_socket.bind(f"tcp://*:{data_port}")
 
+        # get machine configs
+        self.machine_configs = machine_configs
